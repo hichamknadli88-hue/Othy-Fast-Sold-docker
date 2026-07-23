d385c0e9d31ce09d862b49eb9ef84e666bbb6035
hichamknadli88-hue <hichamknadli88@gmail.com>
2026-07-23 20:03:28 +0100

    Update entrypoint.sh

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -8,5 +8,6 @@ mv /etc/nginx/sites-available/default.tmp /etc/nginx/sites-available/default
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+php artisan storage:link --force
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
