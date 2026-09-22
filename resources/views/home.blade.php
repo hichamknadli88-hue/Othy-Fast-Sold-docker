1087ebebdc9ac16a10a075d91e6b3f7bc88b6728
hichamknadli88-hue <hichamknadli88@gmail.com>
2026-09-22 21:23:32 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -289,10 +289,10 @@
     <script>
         // amount, عادي, مع كود OTHY, مع كود paripulse & melbet
         const priceRows = [
-            ['10 DH', '6 Sold', '7 Sold', '8 DH'],
-            ['20 DH', '13 Sold', '14 Sold', '16 DH'],
-            ['50 DH', '33 Sold', '37 Sold', '40 DH'],
-            ['100 DH', '65 Sold', '75 Sold', '80 DH']
+            ['10 DH', '6 DH', '7 DH', '8 DH'],
+            ['20 DH', '12 DH', '14 DH', '16 DH'],
+            ['50 DH', '33 DH', '37 DH', '40 DH'],
+            ['100 DH', '65 DH', '75 DH', '80 DH']
     
         ];
 
